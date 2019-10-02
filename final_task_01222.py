3ef0efb741132708cdc9c45c3c6c4e1dc88dbb29
sachin <sharmasachin1309@gmail.com>
2019-10-02 12:03:02 +0530

    All bugs fixed...

--- final_task_01222.py
+++ final_task_01222.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
 # In[1]:
 
 
@@ -31,8 +43,6 @@ logging.getLogger('scrapy').propagate = False
 # In[2]:
 
 
-
-
 def tag_visible(element):
     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
         return False
@@ -47,6 +57,7 @@ def text_from_html(body):
     return u" ".join(t.strip() for t in visible_texts)
 
 
+# In[3]:
 
 
 keywords_event=['event','book','ticket']
@@ -68,12 +79,12 @@ for i in range(list_len):
         for name in syn.lemma_names():
             keywords_edu.append(name)
 stop = stopwords.words('english') + list(string.punctuation)
-r = requests.get('http://13.71.83.193/api/website-data?count=20') #for now keeping the count as 5
+r = requests.get('http://13.71.83.193/api/website-data?count=20') #for now keeping the count as 20
 data=json.loads(r.text)
 
 
+# In[47]:
 
-# In[4]:
 
 
 websites=[]
@@ -143,25 +154,47 @@ for j in range(len(data)):
         
         api2_update+='group'+str((count))+'='+group_list+'&'+'url'+str((count))+'='+str(website_url)+'&'
 
-    #Adding code for email and contact
+    # for email and phone numbers
         # finding email and contact on main page
         page = requests.get(website_url)
-#         main_contact=page.content
+        main_contact=page.content
 
-#         main_contact_soup = BeautifulSoup(main_contact, 'html.parser')  #Parse html code
-#         text_of_main_contact = main_contact_soup.findAll(text=True)
+        main_contact_soup = BeautifulSoup(main_contact, 'html.parser')  #Parse html code
+        text_of_main_contact = main_contact_soup.findAll(text=True)
 
-#         visible_texts = filter(tag_visible, text_of_main_contact)
-#         main_contact_html_text=u" ".join(t.strip() for t in visible_texts)
+        visible_texts = filter(tag_visible, text_of_main_contact)
+        main_contact_html_text=u" ".join(t.strip() for t in visible_texts)
 
-#         main_contact_mail_list = re.findall('\w+@\w+\.{1}\w+', main_contact_html_text)
-#         main_phone_list=re.findall("1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?",main_contact_html_text)
-#         if main_contact_mail_list is not None:
-#             update+='&email_search=1'
-            
-#         if main_phone_list is not None:
-#             update+='&contact_found=1'
-    
+        main_contact_mail_list = re.findall('\w+@\w+\.{1}\w+', main_contact_html_text)
+        #main_phone_list=re.findall("1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?",main_contact_html_text)
+        main_phone_list=re.findall("|".join(["\+\d\d?-? ?\d{3}-\d{3}-\d{4}","\\+\\d\\d?-? ?\\d{10}","\\+\\d\\d?-? ?\\d{5} \\d{5}","\\+\\d\\d?-? ?\\d{3}\\-? ?\\d{7}","\\+\\d\\d?-? ?\\d{4}\\-? ?\\d{6}"]) ,main_contact_html_text)
+        
+        if len(main_contact_mail_list) is not 0:
+            update+='&email_search=1'
+            print("Email found at main Page: ",main_contact_mail_list)
+            mail_string=''
+
+            for mk in range(len(main_contact_mail_list)):
+                if mk==len(main_contact_mail_list)-1:
+                    mail_string+=str(main_contact_mail_list[mk])
+                else:
+                    mail_string+=str(main_contact_mail_list[mk])+','
+            api2_update+='email'+str(count)+'='+mail_string+'&'
+        if len(main_phone_list) is not 0:
+            update+='&contact_found=1'
+
+            phone_string=''
+            print("Phone number found at main page: ",main_phone_list)
+            for mp in range(len(main_phone_list)):
+                if mp==len(main_phone_list)-1:
+                    phone_string+=str(main_phone_list[mp])
+                else:
+                    #mail_string+=str(main_contact_mail_list[mk])+','
+                    phone_string+=str(main_phone_list[mp])+','
+            api2_update+='phone'+str(count)+'='+phone_string+'&'
+        
+        
+        
         #finding mail and contact in website's contact page
         soup=bs4.BeautifulSoup(page.text,'html.parser')
 
@@ -186,64 +219,68 @@ for j in range(len(data)):
                 html_text=u" ".join(t.strip() for t in visible_texts)
 
                 mail_list = re.findall('\w+@\w+\.{1}\w+', html_text)
-                phone_list=re.findall("1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?",html_text)
+                phone_list=re.findall("|".join(["\+\d\d?-? ?\d{3}-\d{3}-\d{4}","\\+\\d\\d?-? ?\\d{10}","\\+\\d\\d?-? ?\\d{5} \\d{5}","\\+\\d\\d?-? ?\\d{3}\\-? ?\\d{7}","\\+\\d\\d?-? ?\\d{4}\\-? ?\\d{6}"]) ,html_text)
                 
-                if mail_list is not None and 'email_search' not in update:
-                    update+='&email_search=1'
+                
+                #Updating strings for api and api3
+                if len(mail_list) is not 0:
+                    if 'email_search' not in update:
+                        update+='&email_search=1'
                     mail_string=''
+                    
                     for k in range(len(mail_list)):
                         if k==len(mail_list)-1:
                             mail_string+=str(mail_list[k])
                         else:
                             mail_string+=str(mail_list[k])+','
-                    api2_update+='email'+str(count)+'='+mail_string+'&'
-                    #api2_update+='email'+str(count)+'='+
-                if phone_list is not None and 'contact_found' not in update:
-                    update+='&contact_found=1'
-#                     phone_string=''
-#                     for p in range(len(phone_list)):
+                    if 'email'+str(count) not in api2_update:
                         
-#                         j=phone_list[p]
-#                         for k in j:
-#                             b+=k
-#                         if p==len(phone_list)-1:
-                            
-#                             phone_string+=str(b)
-#                         else:
-#                             phone_string+=str(b) + ','
+                        api2_update+='email'+str(count)+'='+mail_string+'&'
+                
+                
+                    
+                if len(phone_list) is not 0:
+                    if 'contact_found' not in update:
+                        update+='&contact_found=1'
+                    
                     phone_string=''
                     for p in range(len(phone_list)):
                         if p==len(phone_list)-1:
                             phone_string+=str(phone_list[p])
                         else:
                             phone_string+=str(phone_list[p])+','
-                    api2_update+='phone'+str(count)+'='+phone_string+'&'
+                    if 'phone'+str(count) not in api2_update:
+                        api2_update+='phone'+str(count)+'='+phone_string+'&'
+                    
                 
-                #print('main url',url)
-                print(link['href'])
-                print(mail_list)
-                print(phone_list)
-                print('-'*50)
+                
+                print("Contact us page link: ",link['href'])
+                if len(mail_list) is not 0:
+                    print("Email found at Contact us page: ",mail_list)
+                if len(phone_list) is not 0:
+                    print("Phone number found at Contact us page: ",phone_list)
                 break
                 
     
     
     
     except Exception as e:
-        print('Invalid website',websites[count-1])
-        r=requests.post('http://13.71.83.193/api/website-data/'+str(i['id'])+'?'+update)
+        print('Invalid website',i['website'])
+        r=requests.post('http://13.71.83.193/api/website-data/'+str(i['id'])+'?'+"crawl_status=2")
+        print('Update for {} website is crawl_status=2'.format(i['website']))
+        print('-'*80)
         j+=1
         continue
     
-    print('Update for this website is',update)
+    print('Update for {} website is {}'.format(website_url,update))
     
     r=requests.post('http://13.71.83.193/api/website-data/'+str(i['id'])+'?'+update)
     if r.status_code==200:
-            print('API3 POST IS SUCCESSFUL')
+        print('API3 POST IS SUCCESSFUL')
     else:
         print('API3 COULD NOT POST')
     count=count+1
-    print('$'*80)
+    print('-'*80)
 #         traceback.print_exc()
 #         fdist1 = FreqDist(filtered_sentence)
 #         print('top_three keyword',str(fdist1.most_common(3))+'---'+str(i))
@@ -264,22 +301,45 @@ for j in range(len(data)):
 #         #traceback.print_exc()  
 
 
-# In[1]:
+# In[ ]:
 
 
-# In[5]:
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[48]:
 
 
 api2_update=api2_update[:-2]
+#api2_update
 
 
-# In[7]:
+# In[50]:
 
-
-api2_update
-
-
-# In[6]:
 
 
 #calling api 2
@@ -288,17 +348,6 @@ if api2_request.status_code==200:
     print('API2 submitted successfullly')
 else:
     print('API2 is not submitted')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
 
